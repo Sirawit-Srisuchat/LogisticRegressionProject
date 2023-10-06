b1d61acf230244784dabd47dcd5754e7ce365006
Sirawit Srisuchat <sirawit.sr.th@gmail.com>
2023-10-06 16:35:19 +0700

    update code

---
+++ LogisticRegression/LogisticRegression/AllClass/LogisticRegression.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticRegression.AllClass
+{
+    public class LogisticRegression
+    {
+        // Variables to store weights, learning rate, and the number of features
+        private double[] weights;  // Weights for each feature (including bias)
+        private double learningRate;  // Learning rate for gradient descent
+        private int numFeatures;  // Number of features
+
+        // Constructor to initialize the logistic regression model
+        public LogisticRegression(int numFeatures, double learningRate = 0.1)
+        {
+            this.numFeatures = numFeatures;
+            this.learningRate = learningRate;
+            weights = new double[numFeatures + 1]; // +1 for the bias term
+        }
+
+        // Train the logistic regression model using gradient descent
+        public void Train(double[][] X, int[] y, int numEpochs)
+        {
+            for (int epoch = 0; epoch < numEpochs; epoch++)
+            {
+                for (int i = 0; i < X.Length; i++)
+                {
+                    // Add bias term and make a prediction for the current example
+                    double[] xWithBias = new double[X[i].Length + 1];
+                    Array.Copy(X[i], 0, xWithBias, 1, X[i].Length);
+                    xWithBias[0] = 1; // Bias term
+
+                    double predicted = Predict(xWithBias);
+
+                    // Compute the error and update the weights using gradient descent
+                    double error = y[i] - predicted;
+
+                    for (int j = 0; j < weights.Length; j++)
+                    {
+                        weights[j] += learningRate * error * xWithBias[j];
+                    }
+                }
+            }
+        }
+
+        // Predict the output for given input features
+        public double Predict(double[] features)
+        {
+            if (features.Length != numFeatures + 1)
+                throw new ArgumentException("Invalid number of features.");
+
+            double score = 0;
+            for (int i = 0; i < features.Length; i++)
+            {
+                score += weights[i] * features[i];
+            }
+
+            return Sigmoid(score);
+        }
+
+        // Sigmoid activation function
+        private double Sigmoid(double x)
+        {
+            return 1.0 / (1.0 + Math.Exp(-x));
+        }
+    }
+}
---
+++ LogisticRegression/LogisticRegression/Program.cs
@@ -0,0 +1,47 @@
+﻿using MathNet.Numerics.LinearAlgebra;  // Importing a library for linear algebra operations
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogisticRegression.AllClass; // Import the corrected namespace
+
+namespace LogisticRegression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sample data (features and labels)
+            double[][] features = new double[][] {
+                new double[] { 2.0, 1.0 },
+                new double[] { 3.0, 1.5 },
+                new double[] { 4.0, 2.0 },
+                new double[] { 3.5, 0.5 },
+                new double[] { 5.0, 2.0 },
+                new double[] { 1.0, 1.0 }
+            };
+
+            int[] labels = new int[] { 0, 0, 0, 1, 1, 1 };
+
+            // Create and train the logistic regression model
+            LogisticRegression.AllClass.LogisticRegression logisticRegression =
+                new LogisticRegression.AllClass.LogisticRegression(numFeatures: 2); // Initialize a logistic regression model
+
+            // Train the logistic regression model using the provided sample data
+            logisticRegression.Train(features, labels, numEpochs: 1000);
+
+            // Test the model
+            double[] testFeatures = new double[] { 2.5, 1.5 };
+            double[] testFeaturesWithBias = new double[testFeatures.Length + 1];
+            Array.Copy(testFeatures, 0, testFeaturesWithBias, 1, testFeatures.Length);
+            testFeaturesWithBias[0] = 1; // Adding a bias term
+
+            // Predict using the trained logistic regression model
+            double prediction = logisticRegression.Predict(testFeaturesWithBias);
+
+            // Display the prediction for the test features
+            Console.WriteLine("Prediction for test features: " + prediction);
+        }
+    }
+}
